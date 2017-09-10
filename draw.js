c70182e1b1dfc63b2369dd19b5a0e78cd7afc077
Haruto Kanno <budoucha@gmail.com>
2017-09-10 17:06:23 +0900

    D3移植先輩

---
+++ draw.js
@@ -0,0 +1,45 @@
+createsvg();
+
+function createsvg() {
+  var width = 640;
+  var height = 480;
+
+  var svg = d3.select("#senpai").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+  var bg1 = svg.append('rect')
+    .attr("fill", rgbToHex(200, 190, 181))
+    .attr("x", 0)
+    .attr("y", 0)
+    .attr("width", width)
+    .attr("height", height);
+
+  var bg2 = svg.append('rect')
+    .attr("fill", rgbToHex(150, 159, 154))
+    .attr("x", width / 2)
+    .attr("y", 0)
+    .attr("width", width / 2)
+    .attr("height", height);
+
+  var path = "M " + width / 2 + " " + height / 2 + " L " + width + " " + height + " H " + 0 + " Z";
+  console.log(path);
+
+  var body = svg.append('path')
+    .attr("d", path);
+
+  var face = svg.append('circle')
+    .attr("cx", width / 2)
+    .attr("cy", height / 2)
+    .attr("r", height / 4)
+    .attr("fill", rgbToHex(161, 114, 106));
+}
+
+function componentToHex(c) {
+  var hex = c.toString(16);
+  return hex.length == 1 ? "0" + hex : hex;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+}
